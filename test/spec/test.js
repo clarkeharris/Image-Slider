961286fffcb03d93804a560cf04a27d4f7b35955
Clarke Harris <clarkeharris@gmail.com>
2014-05-28 08:20:16 -0400

    Added test suite for doc scripts

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,14 +1,30 @@
 /* global describe, it */
 
+
+// [i] has to be less than the array length
+// Fade needs to be a boolean value
+// i has to be >= 0? 
+
 (function () {
     'use strict';
+    var i = 0;
+    describe('Image Looper', function () {
+        it('should loop through an array of images', function () {
+        	expect(i).to.be.below(imgList.length);
+        });
+    });
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-              var answer = 2;
-              expect(2).to.equal(answer);
-            });
+    describe('Fade Animation', function () {
+        it('should fade in and fade out after first image has loaded', function () {
+        	var i = 1;
+        	expect(1).to.be.true;
+        });
+    }); 
+
+    describe('', function () {
+        it('should run here few assertions', function () {
+        	var answer = 2;
+        	expect(2).to.equal(answer);
         });
     });
 })();
