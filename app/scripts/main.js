1aac24069aea23ec86d33c5b9ad99029caf5b33d
Clarke Harris <clarkeharris@gmail.com>
2014-05-27 21:58:39 -0400

    Initial Commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+var imgList = ['https://d13yacurqjgara.cloudfront.net/users/69311/screenshots/1571324/eagle.jpg', 'https://d13yacurqjgara.cloudfront.net/users/245692/screenshots/1571270/african-bookshelf.jpg', 'https://d13yacurqjgara.cloudfront.net/users/124227/screenshots/1571245/lightyear-thumbnail.jpg' ];
+ 
+ 
+function render (i, fade) {	
+	var url = imgList[i];
+	if (fade) 
+	{
+		$('.slider').fadeOut(250, function() 
+		{
+			$('.slider').html('<img src=' +url+ ' class=img-top>');		
+			$('.slider').fadeIn(250);
+		});
+	}
+	else 
+	{
+		$('.slider').html('<img src=' +url+ ' class=img-top>');
+	}
+};
+
+$(document).ready (function () {
+
+	var i = 0;
+	
+	render(i, false);
+	
+	setInterval(function () {
+		
+		i = ++i % imgList.length;
+
+		render(i, true);
+ 
+		}, 5000);
+})
