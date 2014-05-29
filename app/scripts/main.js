2704f834113589a1695fd3b21d842a26fa17d7fb
Clarke Harris <clarkeharris@gmail.com>
2014-05-29 02:06:30 -0400

    Added tests to test.js and implemented constructor function for slideshow in main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,44 @@
 var imgList = ['https://d13yacurqjgara.cloudfront.net/users/69311/screenshots/1571324/eagle.jpg', 'https://d13yacurqjgara.cloudfront.net/users/245692/screenshots/1571270/african-bookshelf.jpg', 'https://d13yacurqjgara.cloudfront.net/users/124227/screenshots/1571245/lightyear-thumbnail.jpg' ];
  
-function slideshow (images, duration) {
+// function slideshow (images, duration) {
+//   if (!$.isArray (images)) throw "Images is not an array";
+
+//   if (images.length == 0) throw "Array is Empty";
+
+//   if (!duration) throw "Duration is not defined";
+
+
+//   var i = 0;
+
+//   setInterval(function () {
+//   	$('.slider').html('<img src=' +images[i]+ ' class=img-top>');	
+//   	i = (i == images.length - 1) ? 0 : i += 1;
+//   }, duration);
+
+// }
+
+// slideshow (imgList, 3000);
+
+
+function SlideShow (images, duration, selector) {
   if (!$.isArray (images)) throw "Images is not an array";
 
   if (images.length == 0) throw "Array is Empty";
 
-  if (!duration) throw "Duration is not defined"
-}
+  if (!duration) throw "Duration is not defined";
 
+this.images = images
+  var i = 0;
+
+  setInterval(function () {
+  	$(selector).html('<img src=' +images[i]+ ' class=img-top>');	
+  	i = (i == images.length - 1) ? 0 : i += 1;
+  }, duration);
+
+}
+var SS = new SlideShow (imgList, 3000, '.slider');
+var TT = new SlideShow (imgList, 1000, '.image');
+var UU = new SlideShow (imgList, null, '.image');
 
 
 // function render (i, fade) {	
@@ -36,4 +67,4 @@ function slideshow (images, duration) {
  
 // 		}, 5000);
 // })
-//counter = (counter ==imageCount-1) ? 0 : counter += 1;
+//counter = (counter == imageCount-1) ? 0 : counter += 1;
